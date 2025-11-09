b6c62720a9f1e3c5713b2efe677089d3628682d5
lcantillo84 <lcantillodev@gmail.com>
2025-11-09 14:15:25 -0500

    Initial commit - project setup

---
+++ MomBabyLogger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MomBabyLogger
+//
+//  Created by Lilianne Cantillo on 11/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MomBabyLogger/MomBabyLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomBabyLoggerApp.swift
+//  MomBabyLogger
+//
+//  Created by Lilianne Cantillo on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MomBabyLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
